4199aa4ea0817d063e2ec4942190539f0b241a5d
Gianluca Geusa <geuluk@gmail.com>
2021-04-29 14:13:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,67 @@
+'use strict';
+
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+var bodyParser = require('body-parser')
+var multer = require('multer');
+var upload = multer({ dest: 'public/uploads/' })
+// require and use "multer"...
 
 var app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  if(req.file) {
+    let file = req.file;
+    res.json({
+      "name": file.originalname,
+      "type": file.mimetype,
+      "size": file.size
+    });
+  } else
+      next({status: 403, message: 'invalid fileupload.'})
+})
 
+// Not found middleware
+app.use((req, res, next) => {
+  return next({status: 404, message: 'not found'})
+})
 
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400 // bad request
+    const keys = Object.keys(err.errors)
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message
+  } else {
+    // generic or custom error
+    errCode = err.status || 500
+    errMessage = err.message || 'Internal Server Error'
+  }
+  res.status(errCode).type('txt')
+    .send(errMessage)
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
 });
